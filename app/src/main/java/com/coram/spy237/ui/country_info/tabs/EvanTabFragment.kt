2bcd9ff5f5b2915c8f70229fe51fc4949c9debd0
jeonmingyun <jun.min.gyun@jjglobal.kr>
2022-10-14 15:56:48 +0900

    테스트 커밋

--- app/src/main/java/com/coram/spy237/ui/country_info/tabs/EvanTabFragment.kt
+++ app/src/main/java/com/coram/spy237/ui/country_info/tabs/EvanTabFragment.kt
@@ -120,6 +120,7 @@ class EvanTabFragment : Fragment() {
             val fragment = EvanTabFragment()
             fragment.arguments = args
             return fragment
+            1111111111111111111111111111111111111111111
         }
     }
 
